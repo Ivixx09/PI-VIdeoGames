d25bbe7cf849fa244bcd8a3689ebd6311a80be41
Ivixx09 <estudio.xx23@gmailcom>
2023-03-16 13:24:38 -0300

    Url fixed

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import LandingPage from './components/LandingPage';
 import Home from './components/Home';
 import CreateGame from './components/CreateGame';
 import { GameDetail } from './components/GameDetail';
-
+import axios from "axios"
+axios.defaults.baseURL = "http://localhost:3001/";
 function App() {
   return (
   <BrowserRouter>
--- client/src/action/index.js
+++ client/src/action/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function getAllVideogames() {
   return async function (dispatch) {
-    const json = await axios.get("http://localhost:3001/videogames");
+    const json = await axios.get("/videogames");
     return dispatch({
       type: "GET_VIDEOGAMES",
       payload: json.data,
@@ -12,7 +12,7 @@ export function getAllVideogames() {
 
 export function getAllPlatforms() {
   return async function (dispatch) {
-    const json = await axios.get("http://localhost:3001/platforms");
+    const json = await axios.get("/platforms");
     return dispatch({
       type: "GET_PLATFORMS",
       payload: json.data,
@@ -22,7 +22,7 @@ export function getAllPlatforms() {
 
 export function getAllGenres() {
   return async function (dispatch) {
-    const json = await axios.get("http://localhost:3001/genres");
+    const json = await axios.get("/genres");
     return dispatch({
       type: "GET_GENRES",
       payload: json.data.map((g) => g.name),
@@ -33,13 +33,13 @@ export function getAllGenres() {
 export function getCreatedGamesOrNot(evento) {
   return async function (dispatch) {
     if (evento === "vCreated") {
-      const json = await axios.get("http://localhost:3001/gamesCreated");
+      const json = await axios.get("/gamesCreated");
       return dispatch({
         type: "GET_VIDEOGAMES_CREATED",
         payload: json.data,
       });
     } else {
-      const json = await axios.get("http://localhost:3001/gamesExisting");
+      const json = await axios.get("/gamesExisting");
       return dispatch({
         type: "GET_VIDEOGAMES_CREATED",
         payload: json.data,
@@ -50,13 +50,13 @@ export function getCreatedGamesOrNot(evento) {
 export function getGamesByAlphabet(evento) {
   return async function (dispatch) {
     if (evento === "asc") {
-      const json = await axios.get("http://localhost:3001/gamesAsc");
+      const json = await axios.get("/gamesAsc");
       return dispatch({
         type: "GET_VIDEOGAMES_ALPHABET",
         payload: json.data,
       });
     } else {
-      const json = await axios.get("http://localhost:3001/gamesDesc");
+      const json = await axios.get("/gamesDesc");
       return dispatch({
         type: "GET_VIDEOGAMES_ALPHABET",
         payload: json.data,
@@ -67,13 +67,13 @@ export function getGamesByAlphabet(evento) {
 export function getGamesByRating(evento) {
   return async function (dispatch) {
     if (evento === "Asc") {
-      const json = await axios.get("http://localhost:3001/ratingAsc");
+      const json = await axios.get("/ratingAsc");
       return dispatch({
         type: "GET_VIDEOGAMES_RATING",
         payload: json.data,
       });
     } else {
-      const json = await axios.get("http://localhost:3001/ratingDesc");
+      const json = await axios.get("/ratingDesc");
       return dispatch({
         type: "GET_VIDEOGAMES_RATING",
         payload: json.data,
@@ -84,7 +84,7 @@ export function getGamesByRating(evento) {
 export function getGamesByGender(evento) {
   return async function (dispatch) {
     const json = await axios.get(
-      `http://localhost:3001/gamesByGender?genre=${evento}`
+      `/gamesByGender?genre=${evento}`
     );
     return dispatch({
       type: "GET_VIDEOGAMES_GENDER",
@@ -96,7 +96,7 @@ export function getGamesByGender(evento) {
 export function getNameVideogame(evento) {
   return function (dispatch) {
     axios
-      .get(`http://localhost:3001/videogames?name=${evento}`)
+      .get(`/videogames?name=${evento}`)
       .then((r) => {
         return dispatch({
           type: "GET_NAME_VIDEOGAME",
@@ -111,14 +111,14 @@ export function postGame(payload) {
   return async function (dispatch) {
     payload.platforms = payload.platforms.toString();
     console.log(payload);
-    const json = await axios.post(`http://localhost:3001/videogames`, payload);
+    const json = await axios.post(`/videogames`, payload);
     return json;
   };
 }
 
 export function gameDetail(id) {
   return async function (dispatch) {
-    const { data } = await axios.get(`http://localhost:3001/videogame/${id}`);
+    const { data } = await axios.get(`/videogame/${id}`);
     return dispatch({
       type: "GET_GAME_DETAIL",
       payload: data,
